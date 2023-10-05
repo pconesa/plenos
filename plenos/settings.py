fdbb136c7f54b9391efb837047aaf7bb5c6c411d
pconesa <p.conesa.mingo@gmail.com>
2023-10-05 19:01:26 +0200

    DB depends on debug mode

--- plenos/settings.py
+++ plenos/settings.py
@@ -76,22 +76,25 @@ WSGI_APPLICATION = 'plenos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "plenos",
-            "passfile": ".pgpass",
-            "client_encoding": "UTF8",
-        },
+# Production DB, for now is a local sqlite
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "OPTIONS": {
+                "service": "plenos",
+                "passfile": ".pgpass",
+                "client_encoding": "UTF8",
+            },
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
